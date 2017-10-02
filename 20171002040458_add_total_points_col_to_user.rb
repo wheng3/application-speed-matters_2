18900a2ade6460435b45acae2f95c4af82b0426f
Wee Jing Heng <wheng3@illinois.edu>
2017-10-02 13:45:33 +0800

    Completed, added index user_id to points table

---
+++ 20171002040458_add_total_points_col_to_user.rb
@@ -0,0 +1,5 @@
+class AddTotalPointsColToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :total_points, :integer, default: 0
+  end
+end
--- app/models/point.rb
+++ app/models/point.rb
@@ -4,4 +4,9 @@ class Point < ActiveRecord::Base
   validates :user, presence: true
   validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :label, presence: true
+  after_create :update_total_points
+end
+
+def update_total_points
+	self.user.update(total_points: self.user.total_points + self.value)
 end
